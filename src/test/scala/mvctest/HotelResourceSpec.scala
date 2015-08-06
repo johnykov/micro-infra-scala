f0b95e0256d4e895fc3d9ea99ee820e4978666c1
Jan Kowalski <jan.kowalski@softwaremill.com>
2015-08-06 11:00:38 +0200

    runner for gradle test task

--- src/test/scala/mvctest/HotelResourceSpec.scala
+++ src/test/scala/mvctest/HotelResourceSpec.scala
@@ -3,17 +3,17 @@ package mvctest
 import mvctest.web.HotelController
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.{FunSuite, Matchers}
+import org.scalatest.{FlatSpec, Matchers}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.ui.ExtendedModelMap
 
 @RunWith(classOf[JUnitRunner])
-class HotelResourceTest extends FunSuite with MicroserviceIntegrationSpec with Matchers {
+class HotelResourceSpec extends FlatSpec with MicroserviceIntegrationSpec with Matchers {
 
   @Autowired
   private val hotelController: HotelController = null
 
-  test("wiring works") {
+  "A controller" should "return a list" in {
     val model = new ExtendedModelMap()
     hotelController.list(model) should be("hotels/list")
   }
